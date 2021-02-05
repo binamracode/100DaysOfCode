26c502f0a83b118a4effe98cccacf91c49778043
binamracode <binamraprogrammer@gmail.com>
2021-02-05 20:05:44 +0545

    Added Day 2

---
+++ todo/src/App.js
@@ -0,0 +1,101 @@
+import "./App.css";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { useState } from "react";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { ButtonGroup, TextField } from "@material-ui/core";
+
+function App() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [todo, setTodo] = useState([]);
+  const [todoToAdd, setTodoToAdd] = useState("");
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDelete = (index) => {
+    console.log(index);
+    handleClose();
+    setTodo(todo.filter((item, item_index) => item_index != index));
+  };
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    setTodo([...todo, todoToAdd]);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="title">Simple TODO App</h1>
+      <form onSubmit={handleAddTodo}>
+        <ButtonGroup
+          size="large"
+          color="primary"
+          aria-label="large outlined primary button group"
+        >
+          <TextField
+            className="todo_field"
+            label="Enter your Todo"
+            onChange={(event) => setTodoToAdd(event.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            className="add_todo_btn"
+            onClick={handleAddTodo}
+          >
+            Add
+          </Button>
+        </ButtonGroup>
+      </form>
+      {todo.length > 0 ? (
+        <TableContainer component={Paper} className="table">
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>S.n.</TableCell>
+                <TableCell>Work</TableCell>
+                <TableCell>Options</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {todo.map((each, index) => (
+                <TableRow key={index}>
+                  <TableCell>{index}</TableCell>
+                  <TableCell>{each}</TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      onClick={() => handleDelete(index)}
+                      color="secondary"
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <p className="no_todo">Add Your Todo First</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
